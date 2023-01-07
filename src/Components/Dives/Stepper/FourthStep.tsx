1a856af0537799db689b4c44981bc687b316c345
AxlSou <axlsoubielle@gmail.com>
2023-01-07 12:29:35 +0000

    Margin added to form subtitles

--- src/Components/Dives/Stepper/FourthStep.tsx
+++ src/Components/Dives/Stepper/FourthStep.tsx
@@ -13,7 +13,11 @@ const FourthStep = () => {
       <h6>Feeling</h6>
       <FormControl fullWidth size="small">
         <InputLabel id="feeling">How did you feel about this dive?</InputLabel>
-        <Select labelId="feeling" id="feeling" label="How did you feel about this dive?">
+        <Select
+          labelId="feeling"
+          id="feeling"
+          label="How did you feel about this dive?"
+        >
           <MenuItem value="Amazing">Amazing</MenuItem>
           <MenuItem value="Good">Good</MenuItem>
           <MenuItem value="Average">Average</MenuItem>
@@ -22,7 +26,7 @@ const FourthStep = () => {
       </FormControl>
       <h6>Comments</h6>
       <Stack spacing={3}>
-      <TextField
+        <TextField
           id="Notes"
           label="Write down the memmories of your dive"
           multiline
@@ -34,7 +38,6 @@ const FourthStep = () => {
           variant="outlined"
           size="small"
           placeholder="Who did you dive with?"
-          required
         />
       </Stack>
     </>
