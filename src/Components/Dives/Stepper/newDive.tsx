0162a5b4450279fafb7160e090663b6f283bb66a
AxlSou <axlsoubielle@gmail.com>
2023-01-07 12:30:19 +0000

    Optional steps and skip button added

--- src/Components/Dives/Stepper/newDive.tsx
+++ src/Components/Dives/Stepper/newDive.tsx
@@ -14,14 +14,45 @@ const steps = ['General Information', 'Conditions', 'Equipment', 'Experience']
 
 const NewDiveForm = () => {
   const [activeStep, setActiveStep] = React.useState(0)
+  const [skipped, setSkipped] = React.useState(new Set<number>());
+
+  const isStepOptional = (step: number) => {
+    return [1, 2, 3].includes(step);
+  };
+  
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
 
   const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1)
-  }
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1)
   }
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
 
   const handleReset = () => {
     setActiveStep(0)
@@ -43,15 +74,26 @@ const NewDiveForm = () => {
   }
 
   return (
-    <>
+    <div>
       <Stepper activeStep={activeStep}>
         {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {}
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: {
+            optional?: React.ReactNode;
+          } = {};
+          if (isStepOptional(index)) {
+            labelProps.optional = (
+              <Typography variant="caption">Optional</Typography>
+            );
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
           return (
             <Step key={label} {...stepProps}>
-              <StepLabel>{label}</StepLabel>
+              <StepLabel {...labelProps}>{label}</StepLabel>
             </Step>
-          )
+          );
         })}
       </Stepper>
       <form>{handleStep()}</form>
@@ -67,23 +109,29 @@ const NewDiveForm = () => {
         </React.Fragment>
       ) : (
         <React.Fragment>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
+        <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            Back
+          </Button>
+          <Box sx={{ flex: '1 1 auto' }} />
+          {isStepOptional(activeStep) && (
+            <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+              Skip
             </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
+          )}
+          <Button onClick={handleNext}>
+            {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
+          </Button>
+        </Box>
+      </React.Fragment>
       )}
-    </>
+    </div>
   )
 }
 
