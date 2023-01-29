da697ff45934f32a19cb7ef4eb825083f158c3d5
AxlSou <axlsoubielle@gmail.com>
2023-01-29 19:46:01 +0000

    Adding title to Dives pages and fetching data from firestore

--- src/Components/Dives/index.tsx
+++ src/Components/Dives/index.tsx
@@ -1,13 +1,35 @@
 import FormDialog from './Dialog/dialog'
 import './index.scss'
+import { collection, DocumentData, getDocs } from 'firebase/firestore'
+import { db } from '../../firebaseConfig'
+import { useAppSelector } from '../../hooks'
+import { useEffect, useState } from 'react'
 
 const Dives = () => {
-
+  const { user } = useAppSelector((store) => store.user)
+  const [logs, setLogs] = useState<DocumentData>([]);
   
+  useEffect(() => {
+    if (user.uid) {
+      const docRef = collection(db, "DivesLogs", user.uid, "Dives")
+      const getUserLogs = async () => {
+        const docSnap = await getDocs(docRef)
+        console.log(docSnap.docs.map((doc) => doc.data()))
+        return setLogs(docSnap.docs.map((doc) => doc.data()))
+      }
+      getUserLogs()
+    }
+  }, [user.uid])
 
   return (
     <div className="dives-background">
-      <FormDialog />
+        <h2 className='title'>Logbook</h2>
+        <FormDialog />
+        <div className='logs-container'>{logs.map((log: DocumentData) => {
+          return (
+          <h1>{log.diveTitle}</h1>
+        )})}
+        </div>
     </div>
   )
 }
