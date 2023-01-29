651c131e57cd30ee2e356052aafdbf694efd680c
AxlSou <axlsoubielle@gmail.com>
2023-01-29 19:49:14 +0000

    Sorting imports in userReducer

--- src/Features/userSlice.ts
+++ src/Features/userSlice.ts
@@ -1,5 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { DocumentData } from 'firebase/firestore'
+import { createSlice } from '@reduxjs/toolkit'
 
 interface User {
   user: {
