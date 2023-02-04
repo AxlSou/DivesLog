e6576901d367f31ad49654ca3134b3a5d9bb8598
AxlSou <axlsoubielle@gmail.com>
2023-02-04 19:12:57 +0000

    Roboto Font

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import '@fontsource/roboto/500.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
