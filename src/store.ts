9fb0bf119573d93b6248c1ba3f2ffd0e77d7409b
AxlSou <axlsoubielle@gmail.com>
2023-02-04 19:12:09 +0000

    Dialog reducer added to store

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import userReducer from './Features/userSlice'
 import formReducer from './Features/formSlicer'
 import stepperReducer from './Features/stepperSlicer'
+import dialogReducer from './Features/dialogSlicer'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     form: formReducer,
-    stepper: stepperReducer
+    stepper: stepperReducer,
+    dialog: dialogReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
